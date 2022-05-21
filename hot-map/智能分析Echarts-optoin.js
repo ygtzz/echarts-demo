f21c5d45dfaec6884d245ede651dcd9a5fac0990
mengff <mengff@mengffdeMacBook-Pro.local>
2022-05-21 23:17:20 +0800

    fix: 柱状图按比例显示,legend颜色,其他样式等

--- hot-map/智能分析Echarts-optoin.js
+++ hot-map/智能分析Echarts-optoin.js
@@ -1,5 +1,6 @@
 console.log('mengff')
 console.log(option)
+let seriesFirst = option.series[0];
 let southProvinces = ['云南', '贵州', '广东', '广西', '海南'];
 if (!window._southMap) {
     window._southMap = echarts.getMap('china').geoJson;
@@ -14,34 +15,53 @@ let shenzhen = JSON.parse(JSON.stringify(guangdong));
 shenzhen.properties.name = '深圳';
 shenzhen.properties.cp[0] += 2.2;
 shenzhen.properties.cp[1] += 1.5;
-shenzhen.id = '440001'
+shenzhen.id = '440300'
 southMap.features.unshift(shenzhen);
 
 echarts.registerMap('china', southMap);
 
+
+let cylinderColor = '#0095FF';
+// let cylinderColor = 'rgba(0,149,255,.5)'
+
 var myOption = {
+    //图例隐藏，默认不选中
+    legend: {
+        show: false,
+        selected: {
+            [seriesFirst.name]: false
+        }
+    },
+    //设置图例的颜色
+    color: ['orange'],
     visualMap: {
         show: false
     },
     tooltip: {
         formatter: function (params) {
+            let item = seriesFirst.data.find(t => t.name.includes(params.name)) || {};
+            let {tooltipItems} = item;
+            let t = {};
+            tooltipItems.forEach(tl => {
+                t[tl[0]] = Number(tl[1]).toFixed(0);
+            })
             let data = [
                 {
                     title: '实时车辆调度', content: [
-                        { text: '全省抢修车辆 / 抢修中车辆：', num: '100 / 16', nuit: '辆' },
-                        { text: '全省抢修车辆 / 抢修中车辆：', num: '100 / 16', nuit: '辆' }
+                        { text: '全省抢修车辆 / 抢修中车辆：', num: `${t['全所抢修车辆']} / ${t['抢修中车辆']}`, nuit: '辆' },
+                        { text: '全省发电车辆 / 抢修中发电车辆：', num: `${t['全所发电车辆']} / ${t['抢修中发电车辆']}`, nuit: '辆' }
                     ],
                 },
                 {
                     title: '故障抢修人员', content: [
-                        { text: '当前值班人员 / 抢修人员：', num: '43 / 10', nuit: '人' },
-                        { text: '路途中人员 / 现场人员：', num: '43 / 10', nuit: '人' }
+                        { text: '当前值班人员 / 抢修人员：', num: `${t['当前值班人员']} / ${t['抢修人员']}`, nuit: '人' },
+                        { text: '路途中人员 / 现场人员：', num: `${t['路途中人员']} / ${t['现场人员']}`, nuit: '人' }
                     ],
                 },
                 {
                     title: '故障抢修人员', content: [
-                        { text: '全省台区 / 影响台区：', num: '54 / 12', nuit: '个' },
-                        { text: '全省用户 / 影响用户：', num: '12124 / 1000', nuit: '户' }
+                        { text: '全省台区 / 影响台区：', num: `${t['全所台区']} / ${t['影响台区']}`, nuit: '个' },
+                        { text: '全省用户 / 影响用户：', num: `${t['全所用户']} / ${t['影响用户']}`, nuit: '户' }
                     ],
                 }
             ];
@@ -57,7 +77,7 @@ var myOption = {
                 let item = `<li>
                                 <div style="font-weight:700;font-size:14px;line-height:1;">
                                     <span style="background: #54B9F4;width:3px;height:15px;display:inline-block;vertical-align:bottom;margin-right:4px;"></span>
-                                    <span style="color: #59BAF2; vertical-align: bottom">实时车辆调度</span>
+                                    <span style="color: #59BAF2; vertical-align: bottom">${t.title}</span>
                                 </div>
                                 <div id="u145_text" class="text">
                                     ${content}
@@ -81,9 +101,9 @@ var myOption = {
     },
     series: [
         {
-            name: '中国',
-            type: 'map',
-            mapType: 'china',
+            name: seriesFirst.name,
+            type: seriesFirst.type || 'map',
+            mapType: seriesFirst.mapType || 'china',
             geoIndex: 0,
             label: {
                 normal: {
@@ -93,7 +113,13 @@ var myOption = {
                 emphasis: {
                     show: true,//对应的鼠标悬浮效果
                 }
-            }
+            },
+            select: {
+                itemStyle: {
+                    areaColor: '#eee'
+                }
+            },
+            data: seriesFirst.data
         }
     ],
     geo: [
@@ -108,26 +134,38 @@ var myOption = {
                 // 图形上的文本标签
                 normal: {
                     show: true,
-                    // width: 150,
                     textStyle: {
                         color: "#fff",
                         fontSize: 20,
                     },
                     formatter: function (params) {
-                        // let item = dataMap.find(t => t.name == params.name) || {};
-                        // let num = item.num || 100;
-                        let num = 141;
+                        let item = seriesFirst.data.find(t => t.name.includes(params.name)) || {};
+                        let num = item.value || '';
+                        if(num){
+                            num = Number(num).toFixed(0);
+                        }
+                        let max = Math.max(...seriesFirst.data.map(t => t.value));
+                        let percent = Math.round(num/max) * 30;
+                        let percents = [];
+                        for(let i=0;i<percent;i++){
+                            percents.push(`{cylindermid| }`)
+                        }
+
                         let str = [
                             `{a|${num}分钟/户}`,
-                            `{cylinder| }`,
-                            params.name
+                            `{cylindertop| }`,
+                            ...percents,
+                            `{cylinderbottom| }`,
+                            `{text|${params.name}}`,
                         ].join('\n');
 
                         if (params.name == '深圳') {
                             str = [
                                 `{a|${num}分钟/户}`,
-                                `{cylinder| }`,
-                                params.name,
+                                `{cylindertop| }`,
+                                ...percents,
+                                `{cylinderbottom| }`,
+                                `{text|${params.name}}`,
                                 `{circle| }`
                             ].join('\n');
                         }
@@ -136,16 +174,36 @@ var myOption = {
                     },
                     rich: {
                         a: {
-                            color: '#000',
+                            color: '#101010',
                             fontSize: 14,
-                            fontWeight: 700
+                            fontWeight: 700,
+                            padding: [5,0,0,0]
                         },
-                        cylinder: {
-                            backgroundColor: 'orange',
-                            borderRadius: 7,
-                            width: 13,
-                            height: 40,
-                            lineHeight: 50
+                        cylindermid: {
+                            backgroundColor: cylinderColor,
+                            height: 1,
+                            width: 11,
+                            borderColor: cylinderColor,
+                            borderWidth: 1
+                        },
+                        cylindertop: {
+                            backgroundColor: cylinderColor,
+                            borderRadius: [5,5,0,0],
+                            height: 10,
+                            width: 11,
+                            borderColor: cylinderColor,
+                            borderWidth: 1
+                        },
+                        cylinderbottom: {
+                            backgroundColor: cylinderColor,
+                            borderRadius: [0,0,5,5],
+                            height: 10,
+                            width: 11,
+                            borderColor: cylinderColor,
+                            borderWidth: 1
+                        },
+                        text: {
+                            padding: [0,0,10,0]
                         },
                         circle: {
                             width: 10,
@@ -153,7 +211,7 @@ var myOption = {
                             backgroundColor: '#fff',
                             borderRadius: 5,
                             borderWidth: 3,
-                            borderColor: 'orange',
+                            borderColor: cylinderColor,
                             lineHeight: 20
                         }
                     }
@@ -186,10 +244,9 @@ var myOption = {
                     borderColor: "#05E9F7",
                     areaColor: "#03679D"
                 },
+                // 高亮时
                 emphasis: {
-                    // 高亮时
                     areaColor: "#407CFB",
-                    // opacity: .7,
                     shadowColor: "rgba(10, 136, 164, 0.5)",
                     shadowOffsetX: 0,
                     shadowOffsetY: 0,
@@ -200,5 +257,27 @@ var myOption = {
         }
     ]
 };
+
+// option.legend = {
+//     show: false,
+//     selected = {
+//         [seriesFirst.name]: false
+//     }
+// };
+// option.legend[0].left = 30;
+// option.legend[0].bottom = 20;
+// option.legend[0].top = 'auto';
+
+// let chartInstance = echarts.getInstanceByDom(document.querySelector('[_echarts_instance_]'));
+// chartInstance.on('legendselectchanged', function (params) {
+//     chartInstance.setOption({
+//         legend:{
+//             selected:{
+//                 [params.name]: true
+//             }
+//         }
+//     })
+//     console.log('点击了', params.name);
+// });
 
 Object.assign(option, myOption);
